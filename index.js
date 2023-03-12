10647189ca45f4f3f8545b3750b28a49018a41a9
Purushottam Khedre <purushottamkhedre123@gmail.com>
2023-03-13 01:10:20 +0530

    change .cdmrc

--- index.js
+++ index.js
@@ -5,7 +5,9 @@ const app = express();
  * Adding your code here
  */
 
-console.log(__dirname);
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
 app.listen(1337, () => {
   console.log("Server started at port 1337");
