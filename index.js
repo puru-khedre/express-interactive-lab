81c59e30a87ae834d7f0dc00c52b693872126e76
Purushottam Khedre <purushottamkhedre123@gmail.com>
2023-03-13 17:19:16 +0530

    add necessary code

--- index.js
+++ index.js
@@ -5,3 +5,14 @@ const saveData = require("./db/saveData");
  * const data = { d1, d2, d3 };
  * saveData(data);
  */
+
+const express = require("express");
+const app = express();
+
+app.get("/", (rq, rs) => {
+  rs.sendFile(__dirname + "/index.html");
+});
+
+app.listen(1337, () => {
+  console.log("Server started succefully");
+});
