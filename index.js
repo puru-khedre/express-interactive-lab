22569501a9f00d5a6c7fc02c7cf10391503c3dc7
Purushottam Khedre <purushottamkhedre123@gmail.com>
2023-03-14 15:32:36 +0530

    refactoring

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  rs.sendFile(__dirname + "/index.html");
+  res.sendFile(__dirname + "/index.html");
 });
 
 app.get("/success", (req, res) => {
